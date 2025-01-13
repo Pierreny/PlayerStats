5e9c3175d7a6b48df2545097af3fbd6a31cf45f2
Pierreny <Pierrejer@merrimack.edu>
2025-01-13 10:28:43 -0500

    Update query.sql

---
+++ query.sql
@@ -0,0 +1,9 @@
+use PlayerOne;
+
+select * from player_metrics;
+select * from coach;
+select * from player WHERE  Current_team='Buffalo Sky';
+select * from teams;
+select* from player;
+
+call addCoach(5015,'Kevin Days','LA');
